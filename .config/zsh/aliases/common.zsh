28513224e57918e20ad6291ea37447a9604374c9
Eric Martin <eric.martin@hey.com>
2025-08-25 12:41:23 +0200

    feat: add claude-work command

--- .config/zsh/aliases/common.zsh
+++ .config/zsh/aliases/common.zsh
@@ -1,6 +1,8 @@
 alias attach="tmux new -A -s personal"
 alias attach-work="tmux new -A -s work"
 
+alias claude-work="CLAUDE_CONFIG_DIR=~/.config/claude-work claude"
+
 alias dot="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias zdot="$EDITOR ~/.zshrc"
 
